d62a5a806e24200495372dce084ec080f8d043d3
Jeroen Gerits <jeroen@gerits.email>
2023-05-18 16:49:35 +0200

    Adding build lib files to the repo

---
+++ lib/interpolate.d.ts
@@ -0,0 +1,5 @@
+declare const interpolate: (str: string, data: {
+    [key: string]: string;
+}, expr: RegExp) => string;
+export default interpolate;
+//# sourceMappingURL=interpolate.d.ts.map
---
+++ lib/isTypeOfString.d.ts
@@ -0,0 +1,3 @@
+declare const isTypeOfString: (value: any) => boolean;
+export default isTypeOfString;
+//# sourceMappingURL=isTypeOfString.d.ts.map
