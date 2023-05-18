64a2343652d2fb51d073f6f9e0388d4225c2b597
Jeroen Gerits <jeroen@gerits.email>
2023-05-18 17:00:28 +0200

    Removing isTypeOfString function

--- src/interpolate.ts
+++ src/interpolate.ts
@@ -1,11 +1,9 @@
-import isTypeOfString from './isTypeOfString'
-
 const interpolate = (
-  str: string,
-  data: { [key: string]: string },
-  expr: RegExp
+    str: string,
+    data: { [key: string]: string },
+    expr: RegExp
 ): string =>
-  str.replace(expr, (match: string, key) =>
-    isTypeOfString(data[key]) ? data[key] : match
-  )
+    str.replace(expr, (match: string, key) =>
+        (typeof data[key] === 'string') ? data[key] : match)
+
 export default interpolate
