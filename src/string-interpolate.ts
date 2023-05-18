419e69bac6472d531098ebd056050759350e4846
Jeroen Gerits <jeroen@gerits.email>
2023-05-19 00:54:26 +0200

    Refactor

--- src/string-interpolate.ts
+++ src/string-interpolate.ts
@@ -1,13 +1,11 @@
 /**
- * Interpolate a string with data
+ * Interpolates a given string `str` by replacing placeholders with corresponding values from the provided object `data`.
  *
- * @param str - string to interpolate
- * @param data - data to interpolate into string
- * @param expr - regular expression to match interpolation keys
- *
- * @returns String with data interpolated
+ * @param {string} str The input string to be interpolated.
+ * @param {Record<string, string>} data An object containing key-value pairs, where each key corresponds to a placeholder in the input string and its value is used to replace the placeholder.
+ * @param {RegExp} [expr=/\{([^}]+)}/g] An optional regular expression pattern that determines how the placeholders are matched. The default pattern matches single curly braces {key}.
+ * @returns {string} The interpolated string with replaced placeholders.
  */
-
 const stringInterpolate = (
   str: string,
   data: Record<string, string>,
