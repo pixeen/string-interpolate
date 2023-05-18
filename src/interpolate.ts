ae0ae0f97859e1d636208aa17a977c213f19ae32
Jeroen Gerits <jeroen@gerits.email>
2023-05-18 16:44:42 +0200

    Changed code formatting

--- src/interpolate.ts
+++ src/interpolate.ts
@@ -1,11 +1,11 @@
 import isTypeOfString from './isTypeOfString'
 
 const interpolate = (
-    str: string,
-    data: { [key: string]: string },
-    expr: RegExp
+  str: string,
+  data: { [key: string]: string },
+  expr: RegExp
 ): string =>
-    str.replace(expr, (match: string, key) =>
-        isTypeOfString(data[key]) ? data[key] : match
-    )
+  str.replace(expr, (match: string, key) =>
+    isTypeOfString(data[key]) ? data[key] : match
+  )
 export default interpolate
