7bac979ac39baf04ba0e9c8cb370a6963d5ae19c
Jeroen Gerits <jeroen@gerits.email>
2023-05-18 23:41:36 +0200

    formatting

--- src/global.ts
+++ src/global.ts
@@ -2,16 +2,16 @@ import interpolate from './interpolate'
 import defaultRegex from './defaultRegex'
 
 declare global {
-    interface String {
-        interpolate(data: { [key: string]: string }, regexp: RegExp): string
-    }
+  interface String {
+    interpolate(data: { [key: string]: string }, regexp: RegExp): string
+  }
 }
 
 String.prototype.interpolate = function (
-    data: {
-        [key: string]: string
-    },
-    regexp = defaultRegex
+  data: {
+    [key: string]: string
+  },
+  regexp = defaultRegex
 ): string {
-    return interpolate(String(this), data, regexp)
+  return interpolate(String(this), data, regexp)
 }
